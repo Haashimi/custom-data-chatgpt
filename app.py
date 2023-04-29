120914de57af9a0c11d7c499134afad6d9f82d15
haashimi <mohameddiini@outlook.com>
2023-04-29 14:22:45 +0300

    app.py

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
-from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from llama_index import SimpleDirectoryReader, GPTSimpleVectorIndex, LLMPredictor, ServiceContext
 from langchain import OpenAI
 import gradio as gr
-import sys
 import os
 
 os.environ["OPENAI_API_KEY"] = ''
 
 def construct_index(directory_path):
-    max_input_size = 4096
     num_outputs = 512
-    max_chunk_overlap = 20
-    chunk_size_limit = 600
-
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="text-davinci-003", max_tokens=num_outputs))
 
-    documents = SimpleDirectoryReader(directory_path).load_data()
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
 
-    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+    docs = SimpleDirectoryReader(directory_path).load_data()
+
+    index = GPTSimpleVectorIndex.from_documents(docs, service_context=service_context)
 
     index.save_to_disk('index.json')
 
@@ -32,7 +28,7 @@ def chatbot(input_text):
 iface = gr.Interface(fn=chatbot,
                      inputs=gr.inputs.Textbox(lines=7, label="Enter your text"),
                      outputs="text",
-                     title="My AI Chatbot")
+                     title="Custom-trained AI Chatbot")
 
 index = construct_index("docs")
 iface.launch(share=True)
